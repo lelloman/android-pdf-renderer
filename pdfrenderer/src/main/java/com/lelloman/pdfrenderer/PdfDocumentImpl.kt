205ceb3c7227156c0085c364c9860c2cb67afb63
lelloman <cerasuolodomenico@gmail.com>
2018-12-07 19:43:56 +0100

    Added scaling to rendering + jitpack

--- pdfrenderer/src/main/java/com/lelloman/pdfrenderer/PdfDocumentImpl.kt
+++ pdfrenderer/src/main/java/com/lelloman/pdfrenderer/PdfDocumentImpl.kt
@@ -1,9 +1,11 @@
 package com.lelloman.pdfrenderer
 
 import android.graphics.Bitmap
+import android.graphics.Rect
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
 import java.io.IOException
+import java.lang.Math.*
 
 class PdfDocumentImpl(parcelFileDescriptor: ParcelFileDescriptor) : PdfDocument {
 
@@ -21,7 +23,23 @@ class PdfDocumentImpl(parcelFileDescriptor: ParcelFileDescriptor) : PdfDocument 
 
     override fun render(bitmap: Bitmap, pageIndex: Int) {
         val page = pdfRenderer.openPage(pageIndex)
-        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+        val pageRatio = page.width / page.height.toFloat()
+        val bitmapRatio = bitmap.width / bitmap.height.toFloat()
+        val (destWidth, destHeight) = if(pageRatio > bitmapRatio) {
+            bitmap.width to round(bitmap.width / pageRatio)
+        } else {
+            round(bitmap.height * pageRatio) to bitmap.height
+        }
+        val horizontalMargin = abs(bitmap.width - destWidth) / 2
+        val verticalMargin = abs(bitmap.height - destHeight) / 2
+
+        val destClip = Rect(
+            horizontalMargin,
+            verticalMargin,
+            max(bitmap.width, destWidth) - horizontalMargin,
+            max(bitmap.height, destHeight) - verticalMargin
+        )
+        page.render(bitmap, destClip, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
         page.close()
     }
 
